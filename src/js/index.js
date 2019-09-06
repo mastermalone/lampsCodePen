2671d49926a32011957d0b1fcd69a43273adc51b
michael.malone <Michael.Malone@vikingcruises.com>
2019-09-05 17:44:57 -0700

    Small update to wrap the services calls in a function.

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
-import { STRING } from './constants.js';
-import stringToArrayService from './stringToArrayService.js';
-import stringCaseService from './stringCaseService.js';
-import getFirstCharacter from './getFirstCharacterService.js';
-import getWordsService from './getWordsService.js';
+import { STRING } from "./constants.js";
+import stringToArrayService from "./stringToArrayService.js";
+import stringCaseService from "./stringCaseService.js";
+import getFirstCharacter from "./getFirstCharacterService.js";
+import getWordsService from "./getWordsService.js";
 
-const wordArray = stringToArrayService.convert(STRING);
-const uppercaseWord = stringCaseService.getCase(wordArray, 'upper');
-const lowercaseWord = stringCaseService.getCase(wordArray);
-const firstCharacterArray = getFirstCharacter(wordArray);
-const startsWithLetter_M = getWordsService.startsWith('M', wordArray);
-const startsWithLetter_T = getWordsService.startsWith('T', wordArray, true);
-console.log('STRING', STRING);
-console.log('wordArray', wordArray);
-console.log('uppercaseWord', uppercaseWord);
-console.log('lowercaseWord', lowercaseWord);
-console.log('firstCharacterArray', firstCharacterArray);
-console.log('Words that start with "m"', startsWithLetter_M);
-console.log('Words that start with "t"', startsWithLetter_T);
+const init = () => {
+  const wordArray = stringToArrayService.convert(STRING);
+  const uppercaseWord = stringCaseService.getCase(wordArray, "upper");
+  const lowercaseWord = stringCaseService.getCase(wordArray);
+  const firstCharacterArray = getFirstCharacter(wordArray);
+  const startsWithLetter_M = getWordsService.startsWith("M", wordArray);
+  const startsWithLetter_T = getWordsService.startsWith("t", wordArray, true);
+  console.log("STRING", STRING);
+  console.log("wordArray", wordArray);
+  console.log("uppercaseWord", uppercaseWord);
+  console.log("lowercaseWord", lowercaseWord);
+  console.log("firstCharacterArray", firstCharacterArray);
+  console.log('Words that start with "m"', startsWithLetter_M);
+  console.log('Words that start with "t"', startsWithLetter_T.length);
+};
+
+init();
